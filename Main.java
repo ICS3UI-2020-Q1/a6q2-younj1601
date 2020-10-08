594651b042d46f1f51dcbd95feea416cbbaf4715
younj1601 <younj1601@wrdsb.ca>
2020-10-08 17:00:20 +0000

    Finished the second task for 2.04

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ *a search algorithm for an array of 10 numbers
+ * @author Jackson
  */
 public class Main {
 
@@ -9,7 +10,30 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //scanner for input
+    Scanner input = new Scanner(System.in);
+
+    //create an array for numbers
+    int[] nums = new int[10];
+
+    //ask for intergers
+    System.out.println("Please enter in 10 integers to put into the array");
+
+    //get numbers from user and store them in array
+    for(int i = 0; i < nums.length; i++){
+      nums[i] = input.nextInt();
+    }
+
+    //ask for number to find
+    System.out.println("Please enter a number to find");
+    int search = input.nextInt();
+
+    //find the number
+    for(int i = 0; i < nums.length; i++){
+      if(nums[i] == search){
+        System.out.println(search  + " is located at index " + (i + 1));
+      }
+    }
     
   }
 }
